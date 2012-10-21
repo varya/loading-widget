a0bba3e70892f37c9889366a705d3c4bc02ac3ae
Varvara Stepanova <toivonen@yandex-team.ru>
2012-10-21 17:01:44 +0400

    Require i-bem block before b-page

--- blocks-desktop/b-page/b-page.deps.js
+++ blocks-desktop/b-page/b-page.deps.js
@@ -1,10 +1,12 @@
 ({
-    shouldDeps: [
+    mustDeps: [
         {
             block: 'i-bem',
             elem: 'dom',
             mods: { init: 'auto' }
-        },
+        }
+    ],
+    shouldDeps: [
         {
             block: 'loading-widget'
         }
