437f82bc26e2ac4638d6f57eb10444bf7b1c33e1
Varvara Stepanova <toivonen@yandex-team.ru>
2012-10-21 17:04:55 +0400

    Link BEMHTML templates for the page

--- pages-desktop/index/index.bemjson.js
+++ pages-desktop/index/index.bemjson.js
@@ -7,6 +7,7 @@
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index', ie: true },
         { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: '_index.bemhtml.js' },
         { elem: 'js', url: '_index.js' },
         { elem: 'meta', attrs: { name: 'description', value: '' }},
         { elem: 'meta', attrs: { name: 'keywords', value: '' }}
