0242495728ccad5c6966315ea0e2b693fbf57935
Varvara Stepanova <toivonen@yandex-team.ru>
2012-10-21 19:28:27 +0400

    Getting urls when a caterogy is selected

--- blocks-desktop/loading-widget/loading-widget.js
+++ blocks-desktop/loading-widget/loading-widget.js
@@ -8,8 +8,31 @@ BEM.DOM.decl('loading-widget', {
 
                 var lWidget = this;
 
-                $('body').on('change', '.category', function(){
-                    console.log(arguments);
+                var getLinks = function(cat, subCat) {
+
+                    var link = '../../blocks-desktop/database/_' + cat + '/database_' + cat + '_' + subCat + '.json';
+
+                    $.getJSON(
+                        link,
+                        function(data) {
+                            BEM.DOM.append(lWidget.elem('urls'), BEMHTML.apply({
+                                block: 'urls',
+                                urls: data
+                            }));
+                        }
+                    )
+
+                }
+
+                $('body').on('change', '.category', function(e){
+
+                    var sel = $(e.target),
+                        cat = sel.find('option:selected').parent('optgroup').attr('label'),
+                        subCat = sel.val();
+
+                    getLinks(cat, subCat);
+
+
                 })
 
                 $.getJSON(
