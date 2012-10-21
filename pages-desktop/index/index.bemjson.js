d9a456c4f21ee1d4669e80716114988f9f4c228b
Varvara Stepanova <toivonen@yandex-team.ru>
2012-10-21 16:48:03 +0400

    Do not produce any content on the page before widget works

--- pages-desktop/index/index.bemjson.js
+++ pages-desktop/index/index.bemjson.js
@@ -10,24 +10,5 @@
         { elem: 'meta', attrs: { name: 'description', value: '' }},
         { elem: 'meta', attrs: { name: 'keywords', value: '' }}
     ],
-    content:[
-        {
-            block: 'header',
-            content: [
-                'header content goes here'
-            ]
-        },
-        {
-            block: 'content',
-            content: [
-                'main content'
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                'footer content goes here'
-            ]
-        }
-    ]
+    content: ''
 })
